a7df69ab4c08aae20c7eef23ee7ad14006bf9ff1
Ryool <sungryool91@gmail.com>
2018-10-18 16:59:06 +0900

    Study

--- 6장/note.py
+++ 6장/note.py
@@ -1,24 +1,6 @@
-student1 = {'학번' : 1000, '이름' : '홍길동', '학과' : '컴퓨터학과'}
-print(student1)
-
-student1['연락처'] = '010-4382-1020'
-
-print(student1)
-
-student1['학과'] = '영문학과'
-
-print(student1)
-
-del(student1['학과'])
-
-print(student1)
-
-print(student1['학번'])
-print(student1['이름'])
-print(student1['연락처'])
-
-print(student1.get('이름'))
-
-print(student1.get('주소'))
-
-student1.key()
+oldList = ['짜장면', '탕수육', '군만두']
+newList = oldList
+print(newList)
+oldList[0] = '짬뽕'
+oldList.append('깐풍기')
+print(newList)
