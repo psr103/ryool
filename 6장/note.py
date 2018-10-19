65b43a278d944932ff6864369dc6f9eeaafdf301
Ryool <sungryool91@gmail.com>
2018-10-19 10:47:33 +0900

    Study

--- 6장/note.py
+++ 6장/note.py
@@ -1,6 +1,5 @@
-oldList = ['짜장면', '탕수육', '군만두']
-newList = oldList
-print(newList)
-oldList[0] = '짬뽕'
-oldList.append('깐풍기')
-print(newList)
+import math
+
+myData = [int(math.pow(2, num)) for num in range(0, 5) if num % 2 != 0]
+
+print(myData)
